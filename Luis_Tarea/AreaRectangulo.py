3ba40ed498279a2b6cbeda5c25bc51bdac4343e9
Luis-Manuel429 <luismanuelnarvaez429@gmail.com>
2026-06-12 17:34:14 -0600

    Archivos de tarea subidos

---
+++ Luis_Tarea/AreaRectangulo.py
@@ -0,0 +1,5 @@
+print("Escibe la medida de la base y la altura de un rectangulo para calcular su area")
+base = int(input("Escribe la base: "))
+altura = int(input("Escribe la altura: "))
+area = base * altura
+print(f"El area del rectangulo es: {area:.2f}")
