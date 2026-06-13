d35a3425e36e6cf1d188a9e5a1b4b986ea249215
Luis-Manuel429 <luismanuelnarvaez429@gmail.com>
2026-06-12 22:05:28 -0600

    Delete promedionota.py

--- promedionota.py
+++
@@ -1,6 +0,0 @@
-print("escribe 3 notas para calcular el promedio")
-n1 = int(input("Escribe la primera nota: "))
-n2 = int(input("Escribe la segunda nota: "))
-n3 = int(input("Escribe la segunda nota: "))
-Promedio = (n1+n2+n3)/3
-print(f"El promedio es: {Promedio:.2f}")
